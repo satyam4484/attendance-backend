1c6349927bafe33fea863453d24262563ab020b4
satyam <satyambsingh93@gmail.com>
2023-09-07 11:17:44 +0530

    m

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const path = require('path');
 require("dotenv").config()
 // require("./src/DB/connection")
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
-app.use(bodyParser.text())
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.raw())
+// app.use(bodyParser.text())
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const userRouter = require("./src/Routes/user.routes");
-const organizationRouter = require('./src/Routes/organization.routes');
-const departmentRouter = require('./src/Routes/department.routes');
-const adminRouter = require("./src/Routes/admin.routes");
-const documentRoutes = require('./src/Routes/document.routes');
+// const userRouter = require("./src/Routes/user.routes");
+// const organizationRouter = require('./src/Routes/organization.routes');
+// const departmentRouter = require('./src/Routes/department.routes');
+// const adminRouter = require("./src/Routes/admin.routes");
+// const documentRoutes = require('./src/Routes/document.routes');
 
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 
 app.get('/api/getuser' ,(req,res)=>{
     res.send({message:"hello saam",working:true})
