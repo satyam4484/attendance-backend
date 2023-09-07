7e884564e47c94d1465d2160a6518682a429e7a9
satyam <satyambsingh93@gmail.com>
2023-09-07 11:25:38 +0530

    db

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require("dotenv").config()
 
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(res => console.log("connected")).catch(error => console.log(error))
 
 module.exports = mongoose;
